c29b60b6b9f472474916b20341ca98931c9fd081
chadhagerty <chadhagerty@gmail.com>
2026-01-03 14:55:48 +0000

    Initial clean Next.js app

---
+++ README.md
@@ -0,0 +1,1 @@
+# inthepitts-discgolf-app
---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>In The Pitts Disc Golf</h1>
+      <p>Course check-in coming soon.</p>
+    </main>
+  );
+}
